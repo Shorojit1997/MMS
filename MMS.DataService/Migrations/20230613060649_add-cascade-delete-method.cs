d6bd02f8796ec319d694cef9c27991a8fd5c1ee3
Shorojit Kumar Sarkar <shorojit.kumar@brainstation-23.com>
2023-06-13 13:11:22 +0600

    fix error of cascade delete

---
+++ MMS.DataService/Migrations/20230613060649_add-cascade-delete-method.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MMS.DataService.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcascadedeletemethod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Months_MonthId",
+                table: "Expenses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Months_MonthId",
+                table: "Expenses",
+                column: "MonthId",
+                principalTable: "Months",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Months_MonthId",
+                table: "Expenses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Months_MonthId",
+                table: "Expenses",
+                column: "MonthId",
+                principalTable: "Months",
+                principalColumn: "Id");
+        }
+    }
+}
